f3b613d971b38d301edfca57055873d3e4f21fbc
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-04-29 14:45:49 -0700

    multi-line commit message,
    ...to check the expanding content thing.
    seems p. cool.

--- remote.tf
+++ remote.tf
@@ -18,24 +18,45 @@
 #   }
 # }
 
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "nicktech"
-    workspaces {
-      name = "minimum-dev"
-    }
-  }
-}
-
-
-
 # terraform {
 #   backend "remote" {
-#     hostname     = "tfe-zone-b0c8608c.ngrok.io"
-#     organization = "shadycorp"
+#     organization = "nicktech"
+#
 #     workspaces {
-#       name = "fly-by-night-dev"
+#       name = "deploy-check"
 #     }
 #   }
 # }
+
+# terraform {
+#   backend "remote" {
+#     hostname     = "staging-app.terraform.io"
+#     organization = "example_corp"
+#
+#     workspaces {
+#       name = "deploy-check"
+#     }
+#   }
+# }
+
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "nick_throwaway2"
+#     workspaces {
+#       name = "terraform-minimum-dev"
+#     }
+#   }
+# }
+
+
+
+terraform {
+  backend "remote" {
+    hostname     = "tfe-zone-b0c8608c.ngrok.io"
+    organization = "shadycorp"
+    workspaces {
+      name = "fuzzer"
+    }
+  }
+}
