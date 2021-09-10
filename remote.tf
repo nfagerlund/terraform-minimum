41ea1d2f88783c0e5fb39ec65b31bee5a8c60a53
Barrett Clark <barrett.clark@hashicorp.com>
2021-09-10 09:48:33 -0500

    Update remote and workspace

--- remote.tf
+++ remote.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "remote" {
-#     hostname = "tfe-zone-b0c8608c.ngrok.io"
-#     organization = "hashicorp"
-#     workspaces {
-#       name = "nothingland"
-#     }
-#   }
-# }
+terraform {
+  backend "remote" {
+    hostname     = "tfcdev-21675e9a.ngrok.io"
+    organization = "hashicorp"
+    workspaces {
+      name = "wk1"
+    }
+  }
+}
 
 # terraform {
 #   backend "remote" {
@@ -18,15 +18,15 @@
 #   }
 # }
 
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "nicktech"
-    workspaces {
-      name = "minimum-dev"
-    }
-  }
-}
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "nicktech"
+#     workspaces {
+#       name = "minimum-dev"
+#     }
+#   }
+# }
 
 
 
