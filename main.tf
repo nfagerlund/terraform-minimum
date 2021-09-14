82ee9edb163774f6e91fee4f0546f1a1af3a79e8
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-14 11:38:37 -0700

    Add map and array outputs, including one key w/ an underscore

--- main.tf
+++ main.tf
@@ -7,10 +7,21 @@ resource "null_resource" "random" {
   }
 }
 
-output "random" {
+output "random_id" {
   value = "Changed to ${null_resource.random.id}"
 }
 
 output "username" {
   value = "Username is ${var.username}. Extra text!!"
+}
+
+output "rollup" {
+  value = {
+    random_id = null_resource.random.id
+    username = var.username
+  }
+}
+
+output "static" {
+  value = ["one", "two"]
 }
