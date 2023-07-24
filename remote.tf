54bb840be0c926948bfcdc2f567d3fef953584bc
Barrett Clark <barrett.clark@hashicorp.com>
2023-07-24 11:24:00 -0500

    Updating config to trigger speculative run

--- remote.tf
+++ remote.tf
@@ -1,34 +1,33 @@
-# terraform {
-#   required_version = "0.12.31"
-#
-#   backend "remote" {
-#     # hostname = "tfcdev-21675e9a.ngrok.io"
-#     hostname     = "app.staging.terraform.io"
-#     organization = "barretto"
-#
-#     workspaces {
-#       # prefix = "app-"
-#       name = "greedy"
-#       # tags = ["abc-def"]
-#     }
-#   }
-# }
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+  cloud {
+    # hostname = "tfcdev-21675e9a.ngrok.io"
+    hostname     = "app.staging.terraform.io"
+    organization = "barretto"
+
+    workspaces {
+      # prefix = "app-"
+      # name = "wk1"
+      tags = ["helloworld"]
+    }
+  }
+}
 
 # terraform {
 #   backend "local" {
 #     # path = "./tf.state"
 #   }
 # }
-terraform {
-  required_version = "~> 1.4"
-  cloud {
-    hostname     = "app.staging.terraform.io"
-    organization = "barretto"
-    workspaces {
-      name = "wk1"
-    }
-  }
-}
+# terraform {
+#   required_version = "~> 1.4"
+#   cloud {
+#     hostname     = "app.staging.terraform.io"
+#     organization = "barretto"
+#     workspaces {
+#       name = "wk1"
+#     }
+#   }
+# }
 # terraform {
 #   backend "pg" {
 #     conn_str = "postgres://barrettclark@localhost/barrettclark?sslmode=disable"
